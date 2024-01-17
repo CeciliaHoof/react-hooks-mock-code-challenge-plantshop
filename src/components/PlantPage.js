51332d8aead0341db6f0f456dbbe051f2659b9ec
Cecilia <ccchimento@gmail.com>
2024-01-17 09:51:35 -0700

    core delivs complete

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -5,16 +5,17 @@ import Search from "./Search";
 
 function PlantPage() {
   const [plantList, setPlantsList] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
 
   useEffect(() => {
     fetch('http://localhost:6001/plants')
       .then(resp => resp.json())
-      .then(data => setPlantsList(data))
-  }, [])
+      .then(data => setPlantsList(data.filter(plant => plant.name.toLowerCase().includes(searchQuery))))
+  }, [searchQuery])
   return (
     <main>
       <NewPlantForm plants={plantList} addPlant={setPlantsList}/>
-      <Search />
+      <Search onSearch={setSearchQuery}/>
       <PlantList plants={plantList}/>
     </main>
   );
